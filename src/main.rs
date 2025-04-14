519ed8a08d2fafe0a6731692460bbea07270e102
Smelly Shrimp <tymek.lamza@op.pl>
2025-04-14 11:51:11 +0200

    added csv reader, Row struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use csv::Reader;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Row {
+    x: i32,
+    y: i32,
+    name: String,
+}
+
+fn read_rows(path: &str) -> Vec<Row> {
+    let mut rd = Reader::from_path(path)
+        .expect("no such file");
+
+    let mut rows = Vec::<Row>::new();
+
+    for res in rd.deserialize() {
+        let row: Row = res.unwrap();
+        rows.push(row);
+    }
+
+    rows
+}
+
 fn main() {
-    println!("Hello, world!");
+    let blocks = read_rows("src/blocks.csv");
+
+    dbg!(blocks);
 }
