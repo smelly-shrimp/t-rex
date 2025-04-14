45f15ffb64fa812ed7583fd9d6b83592e9eea9f7
Smelly Shrimp <tymek.lamza@op.pl>
2025-04-14 12:19:17 +0200

    added divinding image to sub images

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use csv::Reader;
-use image::{ImageBuffer, RgbaImage};
+use image::{DynamicImage, GenericImageView, ImageBuffer, ImageResult, RgbaImage};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 struct Row {
-    x: i32,
-    y: i32,
+    x: u32,
+    y: u32,
     name: String,
 }
 
@@ -23,12 +23,28 @@ fn read_rows(path: &str) -> Vec<Row> {
     rows
 }
 
+fn sub_img(img: &DynamicImage, sx: u32, sy: u32, chunk_size: u32) -> RgbaImage {
+    let mut chunk_img: RgbaImage = ImageBuffer::new(chunk_size, chunk_size);
+
+    for y in 0..chunk_size {
+        for x in 0..chunk_size {
+            let pixel = img.get_pixel(x + sx * chunk_size, y + sy * chunk_size);
+            chunk_img.put_pixel(x, y, pixel);
+        }
+    }
+
+    chunk_img
+}
+
 fn main() {
     let chunk_size = 16;
     let blocks = read_rows("src/blocks.csv");
 
+    let img = image::open("src/blocks.png")
+        .expect("file doesn't exist");
+
     for block in &blocks {
-        let chunk_img: RgbaImage = ImageBuffer::new(chunk_size, chunk_size);
+        let chunk_img = sub_img(&img, block.x, block.y, chunk_size);
 
         chunk_img.save(format!("src/blocks/{}.png", block.name)).unwrap();
     }
