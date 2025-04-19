1fd92416fa4de63df7d7247cfb218e307c0b5243
Smelly Shrimp <tymek.lamza@op.pl>
2025-04-19 22:08:16 +0200

    moved Args to config module

---
+++ src/config.rs
@@ -0,0 +1,15 @@
+use clap::{arg, Parser};
+
+#[derive(Parser, Debug)]
+pub struct Args {
+    #[arg(long, default_value_t = 16)]
+    pub chunk: u32,
+    #[arg(long, default_value_t = String::from("./assets/data.csv"))]
+    pub csv: String,
+    #[arg(long, default_value_t = String::from("./assets/image.png"))]
+    pub asset: String,
+    #[arg(long, default_value_t = String::from("./res"))]
+    pub dir: String,
+    #[arg(long, default_value_t = String::from(""))]
+    pub pack: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,9 @@
+pub mod config;
 pub mod data;
 pub mod img;
 
+use clap::Parser;
 use std::{fs, path::Path};
-use clap::{arg, Parser};
-
-#[derive(Parser, Debug)]
-struct Args {
-    #[arg(long, default_value_t = 16)]
-    chunk: u32,
-    #[arg(long, default_value_t = String::from("./assets/data.csv"))]
-    csv: String,
-    #[arg(long, default_value_t = String::from("./assets/image.png"))]
-    asset: String,
-    #[arg(long, default_value_t = String::from("./res"))]
-    dir: String,
-    #[arg(long, default_value_t = String::from(""))]
-    pack: String,
-}
 
 fn fill_path(path_str: &String) {
     let path = Path::new(&path_str);
@@ -27,7 +14,7 @@ fn fill_path(path_str: &String) {
 }
 
 fn main() {
-    let args = Args::parse();
+    let args = config::Args::parse();
 
     let rows = data::read_rows(&args.csv);
     let img = image::open(args.asset).unwrap();
